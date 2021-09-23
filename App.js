a58a67a24adaaa916d90f774c43b80bc4ae582c2
Erica Carvalho <ericacrcarvalho@gmail.com>
2021-09-22 22:06:02 -0300

    aula

--- App.js
+++ App.js
@@ -1,15 +1,57 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image, TouchableOpacity } from 'react-native';
+import { Foundation, FontAwesome } from '@expo/vector-icons';
+import MyIcon from 'react-native-vector-icons/Ionicons'
 
-export default function App() {
+const App = () => {
+
+  const salvar = () => {
+  }
+
   return (
+
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.text}>Ícones</Text>
+      <Foundation
+        name='unlock'
+        size={40}
+        color='blue'
+      />
+      <FontAwesome
+        name='android'
+        size={30}
+        color='red'
+      />
+      <MyIcon
+        name='contrast-outline'
+        size={20}
+        color='black'
+      />
+      <Image
+        source={require('./slack-icon.png')}
+        style={{width: 20, height: 20}}
+      />
+      <TouchableOpacity
+        onPress={salvar}
+        style={{backgroundColor:'gray', padding: 6, margin: 2, flexDirection:'row'}}>
+          <Foundation
+            name='save'
+            size={20}
+            color='red'
+          />        
+        <Text>Salvar</Text>
+      </TouchableOpacity>
+      <Foundation.Button
+        name='save'
+        backgroundColor='gray'
+        onPress={salvar}
+        borderRadius={30}
+      >Salvar
+      </Foundation.Button>
     </View>
   );
 }
+export default App;
 
 const styles = StyleSheet.create({
   container: {
@@ -18,4 +60,9 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  text: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    margin: 10,
+  }
 });
